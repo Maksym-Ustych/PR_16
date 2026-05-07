343c56491ac7a06678d15732a92b5c5ecf41d308
Максм <m.ustich@istu.edu.ua>
2026-05-07 18:51:28 +0300

    додано ПР 16

---
+++ PR_16/PR_16/PR_16.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+void printName()
+{
+    cout << "Ім'я: Максим\n";
+}
+
+void printGroup()
+{
+    cout << "Група: alk-43\n";
+}
+
+void printSpeciality()
+{
+    cout << "Спеціальність: Інженерія програмного забезпечення\n";
+}
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    system("chcp 65001 > nul");
+
+    printName();
+    printGroup();
+    printSpeciality();
+
+    return 0;
+}
